758d52d978c20279737c331c6beb2533cea367a0
Rosivaldojesus <js.rosivaldo@gmail.com>
2022-05-09 19:00:21 -0300

    Criação da app funcionarios

---
+++ apps/funcionarios/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100, help_text='Nome do funcionário')
+
+    def __str__(self) -> str:
+        return self.nome
